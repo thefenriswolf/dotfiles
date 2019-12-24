bf0c5bd0e5b9991f0f752a112a5607c2b12a7df9
thefenriswolf <stefan.rohrbacher97@gmail.com>
2019-12-24 11:30:16 +0100

    add emacs

---
+++ emacs/.emacs.d/elpa/flymake-hlint-0.2/flymake-hlint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-hlint" "0.2" "A flymake handler for haskell-mode files using hlint" '((flymake-easy "0.1")) :commit "d540e250a80a09da3036c16bf86f9deb6d738c9c" :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/flymake-hlint")
