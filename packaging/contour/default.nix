08523dc9bc414cbe9cebe70c9bb7951d76bdf299
Stefan Rohrbacher <stefan.rohrbacher97@gmail.com>
2020-05-19 11:20:58 +0200

    cleanup

--- packaging/contour/default.nix
+++ packaging/contour/default.nix
@@ -1,3 +1,7 @@
+# TODO: add to pkgs/top-level/all-packages.nix above "copyq" ->
+# contour = libsForQt5.callPackage ../applications/misc/contour { };
+# TODO: remove all comments before submitting to nixpkgs tree
+
 { stdenv, mkDerivation, lib, fetchFromGitHub, pkgconfig, cmake, ninja
 , extra-cmake-modules, libsForQt5, wrapQtAppsHook, qt5, qtbase, kwindowsystem
 , freetype, fontconfig, harfbuzz, pcre-cpp, libGL, glfw, glew, glm, boost, tbb
@@ -5,12 +9,12 @@
 
 mkDerivation rec {
   pname = "contour";
-  version = "alpha-20200418";
+  version = "alpha-20200418"; # TODO: replace with actual version on first release
 
   src = fetchFromGitHub {
     owner = "christianparpart";
     repo = "contour";
-    rev = "857a3fdcfab316c9f521709faad877c06ea0330a";
+    rev = "857a3fdcfab316c9f521709faad877c06ea0330a"; # TODO: replace with actual version on first release
     sha256 = "04d9v4qwv35nf97bibw1xqjr7cr8fnp4p0n0qg6lhdsrxpmqrm57";
     fetchSubmodules = true;
   };
@@ -45,6 +49,6 @@ mkDerivation rec {
     license = licenses.asl20;
     maintainers = with maintainers; [ thefenriswolf ];
     platforms =
-      [ "x86_64-linux" "x86_64-darwin" "x86_64-freebsd" "x86_64-openbsd" ];
+      [ "x86_64-linux" "x86_64-darwin" "x86_64-freebsd" "x86_64-openbsd" ]; # TODO: check if contour build on those platforms
   };
 }
