e3d9014f4132b8d8d18eb410f55debc2f9cbab5f
thefenriswolf <stefan.rohrbacher97@gmail.com>
2023-10-05 18:16:00 +0200

    add ly

--- home-manager/latex.nix
+++
@@ -1,3 +0,0 @@
-{ config, lib, pkgs, ... }: {
-  home.packages = [ pkgs.texlab pkgs.texlive.combined.scheme-full ];
-}
--- home-manager/sway.nix
+++
@@ -1,4 +0,0 @@
-{ config, lib, pkgs, ... }: {
-  programs.zellij.enable = true;
-  home.packages = [ pkgs.ioztat ];
-}
